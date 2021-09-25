f9a222462ce815a6e50c749a5aaf1f5fdc37b736
Rob Blackbourn <rob.blackbourn@googlemail.com>
2021-09-25 10:45:32 +0100

    Moved storage to folder

--- bareasgi_session/storage/session_storage.py
+++ bareasgi_session/storage/session_storage.py
@@ -1,7 +1,6 @@
 """Session Storage"""
 
 from abc import ABCMeta, abstractmethod
-from typing import Dict
 
 
 class SessionStorage(metaclass=ABCMeta):
@@ -26,17 +25,3 @@ class SessionStorage(metaclass=ABCMeta):
             key (str): The session key.
             session (dict): The session data.
         """
-
-
-class MemorySessionStorage(SessionStorage):
-    """Memory session storage"""
-
-    def __init__(self) -> None:
-        """Memory session storage"""
-        self.sessions: Dict[str, dict] = {}
-
-    async def load(self, key: str) -> dict:
-        return self.sessions.setdefault(key, {})
-
-    async def save(self, key: str, session: dict) -> None:
-        self.sessions[key] = session
